83db74a7957768b7db7c893a63cf7c54e42b09fd
HighMid <xstar671@gmail.com>
2024-01-18 13:26:37 -0500

    Commited for save

--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -1,7 +1,7 @@
 import random
 from collections import Counter
-# import pygame
-# from print_functions import delay_print, delay_print_fast, delay_print_sound
+import pygame
+from print_functions import delay_print, delay_print_fast, delay_print_sound
 
 class GameLogic:
     @staticmethod
@@ -68,91 +68,86 @@ class GameLogic:
         # Generate and return a tuple of random integers between 1 and 6, with the number of elements equal to num_dice.
         return tuple(random.randint(1, 6) for _ in range(num_dice))
 
-
-### Below is code I will refactor into a class, will be the text story included with the game
-
-# def config():
+class GameConfig:
     
-#     choice = input("Would you like music to be played? Yes/No: ").strip()
+    def setup_music(self):
     
-#     if choice.lower() in ['yes', 'y']:
-#         pygame.mixer.init()
-#         pygame.mixer.music.load("ten_thousand\Shnabubula - VGMCAST Vol. 8 - 01 Final Fantasy VIII - Shuffle or Boogie.mp3")
-#         pygame.mixer.music.play(-1)
-#         return
-    
-#     if choice.lower() in ['n', 'no']:
+        choice = input("Would you like music to be played? Yes/No: ").strip()
         
-#         print("No worries, maybe next time.")
-#         return
-#     else:
-#         print("No worries, maybe next time.")
-#         return
-    
-# def play_game():
-#     print("We are playing.")
-#     return
+        if choice.lower() in ['yes', 'y']:
+            pygame.mixer.init()
+            pygame.mixer.music.load("ten_thousand\Shnabubula - VGMCAST Vol. 8 - 01 Final Fantasy VIII - Shuffle or Boogie.mp3")
+            pygame.mixer.music.play(-1)
         
-# def rules():
-#     delay_print("Ngl I don't understand it completely so just listen to your heart...")
-#     return
-    
-# def intro():
-    
-#     print()
-#     delay_print("Howdy friend, I see you're awake . . .")
-#     delay_print("Got caught crossing the border too huh . . .")
-#     delay_print("Well we probably don't have that much time until we go to jail.")
-#     print()
-#     delay_print("Let's say we play a game? What do you say? Yes/No")
-#     choice = input()
-    
-#     print()
-#     if choice.lower() in ['yes', 'y']:
-#         delay_print("Hell yeah, I think it's called Farkle or something.")
-    
-#     elif choice.lower() in ['no', 'n']:
-#         delay_print("Understandable, I too like to think about how cozy my prison bed will be.")
-#         delay_print("You take care now, I'll play by myself.")
-#         exit()
-        
-#     else:
-#         delay_print("Sorry I didn't quite get that, but you look like a gamer anyways so we'll just play Farkle.")
-    
-# def who_am_I(asked):
-#     print("I am me")
-#     return
-
-# def menu():
-    
-#     asked = 0
-    
-#     delay_print("Well we got some time to kill, got anything questions before we start?")
-    
-#     while True:
-        
-#         print()
-#         print("\t1.  Play Game")
-#         print("\t2.  Rules")
-#         print("\t3.  Who are you?")
-#         print("\t4.  Quit")
-        
-#         choice = input("Option: ")
-        
-#         if choice == "1" or choice.lower() in ['one']:
-#             play_game()
-#         elif choice == "2" or choice.lower() in ['two']:
-#             rules()
-#         elif choice == "3" or choice.lower() in ['three']:
-#             who_am_I(asked)
-#         elif choice == "4" or choice.lower() in ['four']:
-#             delay_print(". . . well thanks for getting my hopes up, I hope you trip on a lego.")
-#             exit()
-#         else:
-#             delay_print("Sorry I didn't catch that can you repeat that? (Invalid Response)")
+        if choice.lower() in ['n', 'no']:
             
-# def play():
-#     pass
+            print("No worries, maybe next time.")
+        else:
+            print("No worries, maybe next time.")
+
+class Game:
+    
+    def __init__(self):
+        pass
+    
+    def play_game(self):        
+        print("We are playing.")
+
+    def rules(self):        
+        delay_print("Ngl I don't understand it completely so just listen to your heart...")
+
+    def intro(self):
+        print()
+        delay_print("Howdy friend, I see you're awake . . .")
+        delay_print("Got caught crossing the border too huh . . .")
+        delay_print("Well we probably don't have that much time until we go to jail.")
+        print()
+        delay_print("Let's say we play a game? What do you say? Yes/No")
+        choice = input()
+        
+        print()
+        if choice.lower() in ['yes', 'y']:
+            delay_print("Hell yeah, I think it's called Farkle or something.")
+        
+        elif choice.lower() in ['no', 'n']:
+            delay_print("Understandable, I too like to think about how cozy my prison bed will be.")
+            delay_print("You take care now, I'll play by myself.")
+            exit()
+
+        else:
+            delay_print("Sorry I didn't quite get that, but you look like a gamer anyways so we'll just play Farkle.")
+    
+    def who_am_I(self, asked):
+        print("I am me")
+    
+    def menu(self):
+    
+        asked = 0
+        
+        delay_print("Well we got some time to kill, got anything questions before we start?")
+        
+        while True:
+            
+            print()
+            print("\t1.  Play Game")
+            print("\t2.  Rules")
+            print("\t3.  Who are you?")
+            print("\t4.  Quit")
+            
+            choice = input("Option: ")
+            
+            if choice == "1" or choice.lower() in ['one']:
+                self.play_game()
+            elif choice == "2" or choice.lower() in ['two']:
+                self.rules()
+            elif choice == "3" or choice.lower() in ['three']:
+                self.who_am_I(asked)
+            elif choice == "4" or choice.lower() in ['four']:
+                delay_print(". . . well thanks for getting my hopes up, I hope you trip on a lego.")
+                exit()
+            else:
+                delay_print("Sorry I didn't catch that can you repeat that? (Invalid Response)")
+
         
 # def main():
     
